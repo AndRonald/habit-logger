2e8fb9cb8eee983c51bd5bed5a723ee4198fc502
Ronald Araujo <ronald.cortezlouy@gmail.com>
2026-04-20 17:03:03 -0300

    adding methods: addhabit, updatehabit, deletehabit, gethabits

---
+++ HabitLogger/Entities/DataAccess.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace HabitLogger.Entities
+{
+    public static class DataAccess
+    {
+        private readonly static string _connectionString = "Server=(localdb)\\RonaldProjetos;Database=Habits;Trusted_Connection=True;";
+        public static string GetConnectionString()
+        {
+            return _connectionString;
+        }
+    }
+}
